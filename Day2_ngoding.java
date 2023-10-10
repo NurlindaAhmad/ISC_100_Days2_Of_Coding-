96d28af08d806a06a259f41cd347eab5025a093e
NurlindaAhmad <nurlindaahmad58@gmail.com>
2023-10-10 14:03:21 +0700

    first commit

---
+++ Day2_ngoding.java
@@ -0,0 +1,29 @@
+
+package Hari2_ngoding;
+
+
+public class Day2_ngoding {
+    public static void main(String[] args) {
+        
+    
+        byte b = -128;
+        short s = 32767;
+        int i = -2147483648;
+        long l = 9223372036854775807L;
+        float f = 3.14159265f;
+        double d = 2.718281828459045;
+        char c = 'Z';
+        boolean bool = false;
+
+        System.out.println("byte    : " + b);
+        System.out.println("short   : " + s);
+        System.out.println("int     : " + i);
+        System.out.println("long    : " + l);
+        System.out.println("float   : " + f);
+        System.out.println("double  : " + d);
+        System.out.println("char    : " + c);
+        System.out.println("boolean : " + bool);
+    }
+}
+    
+
